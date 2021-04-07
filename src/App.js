1e58f5b326b29cca27902cb463f781799ab8f493
Agwa <agwa.dan@gmail.com>
2021-04-07 20:59:07 +0300

    Complete todo frontend

--- src/App.js
+++ src/App.js
@@ -2,20 +2,45 @@ import './App.css';
 import React, { useState } from 'react';
 
 /* todo component */
-function ToDo({ todo }) {
+const ToDo = ({ todo, index, handleIsComplete, removeTodo }) => {
   return (
-    <div className='todo'>
+    <div className='todo'
+      style={{ textDecoration: todo.isComplete ? 'line-through' : '' }}
+    >
       {todo.text}
+      <button onClick={() => handleIsComplete(index)}>Complete</button>
+      <button onClick={() => removeTodo(index)}>Remove</button>
     </div>
   );
 };
 
-function App() {
-  const [todos, setTodos] = React.useState([
-    { text: 'Start todo' },
-    { text: 'Do todo' },
-    { text: 'Complete todo' },
+const App = () => {
+  const [todos, setTodos] = useState([
+    { text: 'Start todo', isComplete: false },
+    { text: 'Do todo', isComplete: false },
+    { text: 'Complete todo', isComplete: false },
   ]);
+  const [isComplete, setIsComplete] = useState(false);
+
+  //-------ADD ITEM TO LIST-----------------//
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  //-----------UPDATE TASK ISCOMPLETE-----//
+  const handleIsComplete = (index) => {
+    const newToDos = [...todos];
+    newToDos[index].isComplete = true;
+    setTodos(newToDos);
+  }
+
+  //-----------REMOVE ITEM FROM TODO-------//
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
 
   return (
     <div className='app'>
@@ -26,11 +51,35 @@ function App() {
               key={index}
               index={index}
               todo={todo}
+              handleIsComplete={handleIsComplete}
+              removeTodo={removeTodo}
             />
           ))}
       </div>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
+}
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='input'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  )
 }
 
 export default App;
