cc3fd4bed4a2f6c95025473acb5db88bdff06e7a
agwadan <agwa.dan@gmail.com>
2024-01-13 20:42:09 +0300

    Fix blur that remains after adding task

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
 interface Props {
@@ -7,9 +7,18 @@ interface Props {
   handleAdd: (e: React.FormEvent) => void;
 }
 const InputField: React.FC<Props> = ({ todoText, setToDoText, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+        ref={inputRef}
         type="text"
         placeholder="Task Name"
         className="input__box"
