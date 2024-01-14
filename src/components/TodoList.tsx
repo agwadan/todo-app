5eb9dc0128b16bdfe636218363744a7cedc88220
agwadan <agwa.dan@gmail.com>
2024-01-14 22:25:24 +0300

    Add component to display each todo item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
@@ -8,7 +9,7 @@ const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
     <div className="todos">
       {todos.map((todo) => (
-        <li>{todo.todo}</li>
+        <SingleTodo todo={todo} key={todo.id} setTodo={setTodos} />
       ))}
     </div>
   );
